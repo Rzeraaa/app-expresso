e2e6821efbff55a0d8119565f00ed56f326e0984
Renan <renansantosferreira1011@gmail.com>
2026-08-05 07:31:17 -0300

    Remove instalação do driver ODBC (não usado mais com pymssql)

--- render.build.sh
+++ render.build.sh
@@ -2,15 +2,6 @@
 # Exit on error
 set -o errexit
 
-# Instalando as dependências do sistema necessárias para o Microsoft ODBC Driver no Linux
-apt-get update && apt-get install -y curl apt-transport-https debconf-utils
-
-# Baixando e instalando a chave e o repositório oficial da Microsoft para o ODBC Driver 17
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.asc.gpg
-curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
-
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
-
-# Instalando as bibliotecas do Python
+# pymssql já traz as bibliotecas de conexão embutidas no pacote Python,
+# não precisa instalar driver ODBC nem nada no sistema operacional.
 pip install -r requirements.txt
